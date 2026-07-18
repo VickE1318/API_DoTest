6ede9fdb662525daf5d69294b97d2137c2570c05
Vicke <vicke4503@gmail.com>
2026-07-18 17:11:58 +0530

    Add recursive schema validation

--- validators/schema_validator.py
+++ validators/schema_validator.py
@@ -1,18 +1,25 @@
-def validate_schema(actual_data, expected_schema):
+def validate_schema(actual_data, expected_schema,path=""):
     missing=[]
     added=[]
     type_changes=[]
-
     for key in expected_schema:
+        current_path = f"{path}.{key}" if path else key
         if key in actual_data:
-            actual_type = type(actual_data[key]).__name__
-            if actual_type != expected_schema[key]:
-                type_changes.append({"field":key, "expected": expected_schema[key], "actual":actual_type})
+            if isinstance(actual_data[key],dict) and isinstance(expected_schema[key],dict):
+                nested_result=validate_schema(actual_data[key],expected_schema[key],current_path)
+                missing.extend(nested_result["missing"])
+                added.extend(nested_result["added"])
+                type_changes.extend(nested_result["type_changes"])
+            else:
+                actual_type = type(actual_data[key]).__name__
+                if actual_type != expected_schema[key]:
+                    type_changes.append({"field":current_path, "expected": expected_schema[key], "actual":actual_type})
         else:
-            missing.append(key)
+            missing.extend(key)
     for key in actual_data:
+        current_path = f"{path}.{key}" if path else key
         if key not in expected_schema:
-            added.append(key)
+            added.extend(current_path)
 
     return {"missing":missing,"added":added,"type_changes":type_changes}
     
